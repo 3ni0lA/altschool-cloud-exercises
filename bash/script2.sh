b53d078d103409ba44bf0a994905a1f0a12200b1
Adewale Eniola <104868415+3ni0lA@users.noreply.github.com>
2023-01-09 20:36:18 +0000

    added second script

--- bash/script2.sh
+++ bash/script2.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+
+# command to update and upgrade our servers
+
+sudo apt update && sudo apt dist-upgrade -y
+
+
+# the command to install nginx
+
+sudo apt install nginx -y
+
+
+# the command to overide the nginx html page
+
+echo "<h1>This is my Nginx-2 server $(hostname -f)</h1>" > /var/www/html/index.nginx-debian.html
+
+#the command to enable nginx and restart nginx
+
+sudo systemctl enable nginx && sudo systemctl restart nginx
 
